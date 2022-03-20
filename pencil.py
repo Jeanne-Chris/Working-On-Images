953c66227637f8d0fded133d704f87d0cfc240fe
JoshJeremy <69834505+Jeanne-Chris@users.noreply.github.com>
2022-03-20 10:38:31 +0800

    Update pencil.py

--- pencil.py
+++ pencil.py
@@ -9,12 +9,12 @@ img_var = 'image_filename_here.jpg'
 # Load the image and display the image to be converted
 image1 = cv2.imread(img_var, cv2.IMREAD_UNCHANGED)     # Load the image from variable
 frame1_title = 'Original image : '+str(img_var)        # window frame title
-cv2.imshow(frame1_title, image1)                          # display the image from variable
+cv2.imshow(frame1_title, image1)                       # display the image from variable
 
 # convert the image to gray scale
 img2gray = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
 img_new = cv2.bitwise_not(img2gray)
-blur = cv2.GaussianBlur(img_new, (21, 21), 0)     # smoothens image with GaussianBlur()
+blur = cv2.GaussianBlur(img_new, (21, 21), 0)        # smoothens image with GaussianBlur()
 # blur = cv2.bilateralFilter(img_new, 9, 75, 75)     # smoothens image with bilateralFilter()
 invertedblur = cv2.bitwise_not(blur)
 sketch = cv2.divide(img2gray, invertedblur, scale = 256.0)
@@ -22,11 +22,11 @@ sketch = cv2.divide(img2gray, invertedblur, scale = 256.0)
 fname=input("\nType in file name to save: ")
 new_var = str(fname)+'.png'                           # store new file to variable
 print("Saved to file name "+new_var)
-cv2.imwrite(new_var, sketch)                         # save the image to new image file
-image2 = cv2.imread(new_var)                      # read the new image file
-frame2_title = 'Sketch image : '+str(new_var)     # new window frame title
+cv2.imwrite(new_var, sketch)                          # save the image to new image file
+image2 = cv2.imread(new_var)                          # read the new image file
+frame2_title = 'Sketch image : '+str(new_var)         # new window frame title
 
 # display the new image
-cv2.imshow(frame2_title, image2)                  # display the sketched new image
-cv2.waitKey(0)                                                # use waitkey() to prevent Python kernel to crash
-cv2.destroyAllWindows()                                # close all windows
+cv2.imshow(frame2_title, image2)                      # display the sketched new image
+cv2.waitKey(0)                                        # use waitkey() to prevent Python kernel to crash
+cv2.destroyAllWindows()                               # close all windows
